3e1cf9898eb6dd7571ee7d5c3ffc0e234a37931f
hy <hy@localhost>
2023-02-14 12:21:46 -0700

    Pass params for par as file

--- workloads/compositions/parallel.js
+++ workloads/compositions/parallel.js
@@ -1,14 +1,11 @@
 const composer = require('openwhisk-composer')
 
-module.exports = composer.let(
-    {"$composer": {"redis": {"uri": "redis://redis:6379"}}},
-    composer.parallel(
-        composer.action('par1', {
-            action: function (params) {
-                console.log($composer)
-                return { msg: 'par1 invoked' }
-            }
-        }),
-    )
+module.exports = composer.parallel(
+    composer.action('par1', {
+        action: function (params) {
+            console.log(params)
+            return { msg: 'par1 invoked' }
+        }
+    }),
 )
 
